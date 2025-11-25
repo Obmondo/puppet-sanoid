ef66011b51fc98fd067be29cca7ac00115cf10ac
Ashish Jaiswal <ashish@obmondo.com>
2025-11-25 09:08:41 +0530

    fixes:
    * Change to correct data type and use stdlib::ensure function
    * Change the define name to diff other then the data type to avoid, resource duplication error
    * Handle creation of /etc/sanoid dir

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,15 +1,24 @@
 # Sanoid
 class sanoid (
-  Stdlib::Ensure::Package $ensure_package,
-  Stdlib::Ensure::Service $ensure_service,
-  String                  $config_file,
-  String                  $package_name,
-  Sanoid::Pools           $pools,
-  Sanoid::Templates       $templates,
+  Boolean $ensure_package,
+  Boolean $ensure_service,
+  String  $config_file,
+  String  $package_name,
+
+  Sanoid::Pools     $pools,
+  Sanoid::Templates $templates,
+
+  Sanoid::Syncoid::Replications $replications,
 ) {
 
   include sanoid::install
   include sanoid::config
   include sanoid::service
   include sanoid::syncoid
+
+  $replications.each |String $pool_name, Sanoid::Syncoid::Replication $options| {
+    sanoid::syncoid::job { $pool_name:
+      * => $options,
+    }
+  }
 }
