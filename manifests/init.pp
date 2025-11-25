26f9a0d2027f45a47bb081a55d6e5ee61d906422
Ashish Jaiswal <ashish@obmondo.com>
2025-11-25 12:10:08 +0530

    feat: * Added required dependency * Added SSH authorized support

--- manifests/init.pp
+++ manifests/init.pp
@@ -5,20 +5,16 @@ class sanoid (
   String  $config_file,
   String  $package_name,
 
+  Array[String]     $allow_sync_from,
   Sanoid::Pools     $pools,
   Sanoid::Templates $templates,
 
   Sanoid::Syncoid::Replications $replications,
+
 ) {
 
   include sanoid::install
   include sanoid::config
   include sanoid::service
   include sanoid::syncoid
-
-  $replications.each |String $pool_name, Sanoid::Syncoid::Replication $options| {
-    sanoid::syncoid::job { $pool_name:
-      * => $options,
-    }
-  }
 }
